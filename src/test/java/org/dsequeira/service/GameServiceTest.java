449171eb57a3a259d6474aae41f372a8697ae313
danielsequeira <danielsequeira29@gmail.com>
2020-09-30 13:35:30 +0200

    removed unused imports

--- src/test/java/org/dsequeira/service/GameServiceTest.java
+++ src/test/java/org/dsequeira/service/GameServiceTest.java
@@ -7,8 +7,6 @@ import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.Arrays;
 
